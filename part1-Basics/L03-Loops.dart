2a0786fe6a37af04ce7e5c2fb93b5e37ac71b0f5
whoshyam <shyamk2712@gmail.com>
2023-09-14 00:56:30 +0530

    f

--- part1-Basics/L03-Loops.dart
+++ part1-Basics/L03-Loops.dart
@@ -1,47 +1,59 @@
 void main() {
-  //loops
-
-  //for loops
+//!--------for loops
 //   for(init; cond; increment/decrement){
 
 //   }
-//   for(int i=0;i<10;i++){
+//   for(int i=0;i<5;i++){
 //     print('hello ${i+1}');
-
 //   }
-  print('-----------');
-  //extract something from string
-  for (int i = 0; i < 10; i++) {
+// hello 1
+// hello 2
+// hello 3
+// hello 4
+// hello 5
+
+  //!--------extract something from string
+  for (int i = 0; i < 5; i++) {
     String hi = 'hello world';
     print(hi.substring(1, 5)); //numbering of string starts from o
-
     print('hello ${i + 1}');
   }
+// ello
+// hello 1
+// ello
+// hello 2
+// ello
+// hello 3
+// ello
+// hello 4
+// ello
+// hello 5
 
   String value = 'Hello';
   for (int i = 0; i < value.length; i++) {
     print(value[i]);
   }
+// H
+// e
+// l
+// l
+// o
 
-  //while loop
+  //!--------while loop
   int i = 0;
   while (i < value.length) {
     print(value[i]);
     i++;
   }
-  print('--------------');
-
-  //do while loop
+ 
+  //!-------do while loop
   int j = 0;
   do {
     print(value[j]);
     j++;
-  } while (j != j); //always a false condition
+  } while (j != j); //always a false condition so H will be printed only
 
-  print('--------------');
-
-  //continue
-  // print only ello
+  //!---------continue
   String Value = 'Hello';
   for (int z = 0; z < Value.length; z++) {
     if (z == 1 || z == 2 || z == 3) {
@@ -50,4 +62,6 @@ void main() {
     }
     print(Value[z]);
   }
+//H
+//o
 }
