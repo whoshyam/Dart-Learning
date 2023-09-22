4665a5769bd171cffcbf5acd83d6d437ad3dee26
whoshyam <shyamk2712@gmail.com>
2023-09-23 00:16:03 +0530

    update

---
+++ part2-functions/L03-return_nullableValue.dart
@@ -0,0 +1,9 @@
+void main(){
+  var name = printName();
+  print(name);
+}
+String? printName(){
+  return null;
+  //print('hello');
+  //dead code after return statement,return ends the fn after returning the value 
+} 
