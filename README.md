e48c7b050b9c04184424eb245e579454072fe13b
whoshyam <shyamk2712@gmail.com>
2023-09-23 15:00:28 +0530

    a

--- README.md
+++ README.md
@@ -1,60 +1,14 @@
-# Dart-Learning
-## contents
-Print Statement</br>
-Operators</br>
-Comments</br>
-Variables</br>
-var, const and final Variables</br>
-final vs const variables</br>
-Optional Variables/ Nullable Variables</br>
-If Condition</br>
-Ternary Operators</br>
-Switch Statement</br>
-For Loop</br>
-While Loop</br>
-Do While Loop</br>
-continue keyword</br>
-break keyword</br>
-Functions</br>
-Returning 2 Values from a Function (Records)</br>
-Patterns</br>
-Returning Nullable Variables/Values</br>
-Function Positional Arguments & Global Variables</br>
-Function Named Arguments</br>
-Named Records</br>
-Returning Functions from Functions</br>
-Arrow </br>
-Anonymous Functions</br>
-Classes</br>
-Constructor</br>
-this keyword</br>
-Positional Constructor Arguments</br>
-Immutable Classes</br>
-Named Constructor Arguments</br>
-Classes Terminology</br>
-Private Variables</br>
-Getters</br>
-Setters</br>
-Static Variables</br>
-Static Functions</br>
-When to use static keyword?</br>
-Inheritance</br>
-implements keyword</br>
-Abstract Classes</br>
-Object Oriented Programming (OOP) in Dart</br>
-Polymorphism</br>
-Abstraction</br>
-Encapsulation</br>
-OOP Brief</br>
-Mixins</br>
-Class Modifiers</br>
-Lists</br>
-Sets</br>
-Maps</br>
-Enums</br>
-Enhanced Enums</br>
-Exception Handling</br>
-Futures (Promises)</br>
-Streams</br>
-Creating Records</br>
-Patterns & Pattern Matching </br>
+```
+📦 Dart
+├─ README.md
+├─ part1-Basics
+│  ├─ L01-variables.dart
+│  ├─ L02-conditionals.dart
+│  └─ L03-Loops.dart
+└─ part2-functions
+   ├─ L01-fn_intro.dart
+   ├─ L02-returning_multiple_value.dart
+   ├─ L03-return_nullableValue.dart
+   ├─ L04-fn_positional_arguments_globalVariable.dart
+   └─ fn.dart
+```
--- part2-functions/fn.dart
+++ part2-functions/fn.dart
@@ -1,38 +1,13 @@
-/*
-//patterns
- void main(){
-  var (age, name, isAdult,greeting)=printName(); 
-   print(name);
-   print(age);
-   print(isAdult);
-   print(greeting);
-  
-}
 
-(int, String, bool, String) printName(){
-  return (12, 'Hello', true, 'Hi');
-}         
-          
-*/          
-
-
-//---------------------------------------
-/*          
-//fn positional arguments and global variables 
-//String name= 'shyam'; //global variable 
 void main(){
-   String name= 'shyam';
+  //String name= 'shyam';
   var name =printName(); 
   print(name);
-  
 }
-
 String? printName(){
-  print('Hello')
+  print('Hello');
   return'Hi';
 }
-  */
-    
 //---------------------------------------
 
 /*
@@ -45,10 +20,7 @@ void printName(String name, int age, String greeting){ //positional argumment
   print(name);
 }
 */
-
-
 //--------------------------------
-
 /*
 //function named arguments 
  void main() {
@@ -78,7 +50,7 @@ void printName(int age, bool isAdult, {required String name,required String gree
 */
 //-----------------------------
 /*
-//named records 
+//named records
 void main(){
   final stuff = printStuff();
   
@@ -91,47 +63,3 @@ void main(){
   return (age:12, name:'shyam');
 }
 */
-//-------------------------
-
-/*
-//returning function from function 
-void main(){
-  final stuff = printStuff();
-  stuff();
-  ()
-  {
-    print('Yooo');
-  }
-  ();
-}
-
-Function printStuff(){
-  return(){
-    print('yahoo');
-  };
-}
-*/
-
-//-----------------------------
-/*
-//arrow function
-void main(){
-  final stuff = printStuff();
- 
-}
-
-void printStuff() => print('Hi');
-
-*/
-
-//---------------------
-
-//anonymous function
-// fn that dont have any name but behave like a fn (block of code that can be executed)
-//are anonymous fn
-
-//example
-//{
-//print('Hey');
-//}
-
