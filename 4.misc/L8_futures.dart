374bb0b6dfc8af90082d1ba58f2720107802d100
whoshyam <shyamk2712@gmail.com>
2024-03-02 14:17:37 +0530

    init

--- 4.misc/L8_futures.dart
+++ 4.misc/L8_futures.dart
@@ -1,4 +1,17 @@
-void main(){
+void main() async {
 
-  
+    //promises
+
+    final result = await giveAREsultAfter2Sec();
+    print(result);
+    giveAREsultAfter2Sec();
+
+    print('hey');
+    print('hello');
+    print('hi');
+
+}
+
+Future<String> giveAREsultAfter2Sec() async {
+      return 'heyyyy';
 }
